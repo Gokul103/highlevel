2989c033f2ca7282fa2b50e3025ee9e6df55c9c7
Gokul <gokul@clanofcoderz.com>
2024-10-08 10:01:25 +0530

    Adding firebase connection

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require("express");
 const oApp = express();
-
 const port = 5000;
+
+
+
+const firebaseAdmin = require("firebase-admin");
+const credentials = require("./firebase.json");
+
+
+firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert(credentials)
+});
+
+
+const appointmentRouter = require("./router/appointmentRouter");
+oApp.use("/appointment", appointmentRouter);
+
+
 
 oApp.listen(port, () => {
     console.log(`Application started listening on port - ${port}`)
